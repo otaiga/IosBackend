3836d8361d744808a3677b41c195360cebc3e3b9
otaiga <karl.turner5@gmail.com>
2012-11-19 15:51:41 +0000

    added token authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,21 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+
+
+  before_create :ensure_authentication_token, :write_auth_token
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+
+private 
+
+  # get auth token passed in response from devise.
+  def write_auth_token
+    Devise::Models::Authenticatable::BLACKLIST_FOR_SERIALIZATION.delete_if{|x| x == :authentication_token}
+  end
+
 end
---
+++ db/migrate/20121119154316_addtokentodevise.rb
@@ -0,0 +1,11 @@
+class Addtokentodevise < ActiveRecord::Migration
+
+  def up
+    add_column :users, :authentication_token, :string
+  end
+
+  def down
+    remove_column :users, :authentication_token
+  end
+
+end
